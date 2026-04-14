da353cf9ddd569b3af3dcc047e36a0828478bc43
Rasheed Montes <montesrasheed@gmail.com>
2026-04-14 17:51:17 +0800

    prefinal-exam

---
+++ Pre-Final-Exam/app/index.tsx
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export default function Index() {
+  return (
+    <View
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      //   backgroundColor: "black",
+        
+        
+      // }}
+      style={styles.container}
+    >
+      <Text style={styles.txt}>Rasheed yule B. Montes BSIT-2B .</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+     
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+  
+  },
+
+  txt:{
+    color:"white",
+    fontSize: 30
+  }
+
+})
