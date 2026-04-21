2410f35a2bdfbf5abb8138f7bb25240240be6f6e
Phoebe Bardillon <internaleternally0@gmail.com>
2026-04-21 18:16:31 +0800

    Added txt file

--- Pre-Final-Exam/app/index.tsx
+++ Pre-Final-Exam/app/index.tsx
@@ -2,35 +2,23 @@ import { View, Text, StyleSheet } from "react-native"
 
 export default function Index() {
   return (
-    <View
-      // style={{
-      //   flex: 1,
-      //   justifyContent: "center",
-      //   alignItems: "center",
-      //   backgroundColor: "black",
-        
-        
-      // }}
-      style={styles.container}
-    >
-      <Text style={styles.txt}>Rasheed yule B. Montes BSIT-2B .</Text>
+    <View style ={styles.container}>
+      <Text style={styles.text}>Phoebe R. Bardillon BSIT 2B</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container:{
-     
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "black",
-  
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text:{
+    color: 'white',
+    fontSize: 30
   },
 
-  txt:{
-    color:"white",
-    fontSize: 30
-  }
-
-})
+});
+      
